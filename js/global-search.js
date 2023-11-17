cf0cf9804c6a286546e4991b09f7f57e9c63e8ab
Jitendra Nayak <jitendra.nayak@petonic.in>
2023-11-17 07:00:14 +0530

    1st

---
+++ js/global-search.js
@@ -0,0 +1,122 @@
+$(document).ready(function() {
+    $('#business-scenerio').hide();
+     // Configure/customize these variables.
+    var showChar = 100;  // How many characters are shown by default
+    var ellipsestext = "";
+    var moretext = "Read More";
+    var lesstext = "Read Less";
+    
+
+    $('.more').each(function() {
+        var content = $(this).html();
+ 
+        if(content.length > showChar) { 
+            var c = content.substr(0, showChar);
+            var h = content.substr(showChar, content.length - showChar); 
+            var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>'; 
+            $(this).html(html);
+        }
+ 
+    });
+ 
+    $(".morelink").on('click',function(){
+        if($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).text(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).text(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+
+// 
+
+$('#Create-Scenario').click(function(){
+    $('#business-scenerio').show();
+    $(".title-befor-content-load").hide();
+});
+
+
+//
+
+$(".idea-box").click(function(){
+    alert('run');
+    // Hide all table data
+    //$(".table-data").hide();
+  
+    // Get the target ID from the data attribute
+    var targetId = $(this).data("target");
+
+   // $(".idea-box").hide();
+   $('#challenges-box').hide();
+   
+    $(".title-befor-content-load").hide();
+    $("#business-scenerio").hide();
+
+
+
+    // Show the corresponding table data
+    $("#" + targetId).show();
+});
+
+// on click create challenge open nav tab business sceniro
+
+$('#create-challenge').click(function(){
+    $('#open-business-scenerio').show();
+    $('#create-challenge').hide();
+});
+
+// on click next open challenge creation tab
+
+
+
+});
+
+
+//
+
+// Function to show the button when the checkbox is checked
+// document.getElementById('checkBox').addEventListener('change', function () {
+//     var showButton = document.getElementById('showButton');
+//     showButton.style.display = this.checked ? 'block' : 'none';
+//   });
+
+  // Function to open the popup
+//   function openPopup() {        
+//     var popup = document.getElementById('popup');
+//     popup.style.display = 'block';
+//   }
+
+  // Function to close the popup
+//   function closePopup() {
+//     var popup = document.getElementById('popup');
+//     popup.style.display = 'none';
+//   }
+
+
+
+  // tab navigation ob create business scenirao 
+  function changeTab() {
+
+    $('#challenges-box').show();
+    // Get the currently active tab
+    var activeTab = document.querySelector('.nav-tabs .nav-link.active');
+
+    // Get the target tab id
+    var targetTabId = activeTab.getAttribute('data-bs-target');
+
+    // Remove the 'active' class from the current tab and corresponding content
+    activeTab.classList.remove('active');
+    document.querySelector(targetTabId).classList.remove('show', 'active');
+
+    // Set the next tab as active
+    var nextTab = document.getElementById('data2'); // Change this to the desired tab
+    nextTab.classList.add('active');
+
+    // Show the corresponding content for the next tab
+    var nextTabContent = document.getElementById('challenge-creation'); // Change this to the desired content
+    nextTabContent.classList.add('show', 'active');
+  }
